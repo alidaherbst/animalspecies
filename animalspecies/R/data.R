aa90d4696a293e34cd60f2a2fc0fd7fd2ea0f2c5
alidaherbst <alidaherbst55@gmail.com>
2019-10-16 14:24:58 +0200

    Add files via upload

---
+++ animalspecies/R/data.R
@@ -0,0 +1,20 @@
+#' @title animalspecies
+#'
+#' @description This package draws a boxplot for different chosen animal types, gives data for any animal available and draws plots comparing weight and speed
+#'
+#' @param symbol
+#'
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{Number}{A number from from 1 to 108}
+#'  \item{Animal}{Animal name.}
+#'  \item{Category}{Regions: Coast, Forest, Fynbos, Nama Karoo, Ocean, Savanna, Succulent Karoo, Thicket, Wetland}
+#'  \item{Species}{Species: Amphibian, Bird, Crustacean, Fish, Insect, Mammal, Reptile.}
+#'  \item{Age}{Maximum age between 0.05 and 100 years.}
+#'  \item{Weight}{Weight between 0.00001 and 80000.}
+#'  \item{Size}{A value between 0.3 and 1800.}
+#'  \item{Speed}{A value between 0.5 and 160 kilometers per hour.}
+#'  \item{Vulnerability}{An integer from 1 to 4 where 1 is very vulnerable.}
+#' }
+#' "superanimals"
